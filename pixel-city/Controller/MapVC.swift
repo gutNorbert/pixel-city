17c5396372c369f93c8044d450eeee5464c9565f
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-26 18:54:19 +0100

    first commit

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Gutpinter Norbert on 2017. 11. 26..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
